def9773f9ab1f42f16cde7051a5456e17c7500b2
ali raza <ali.raza@justads.com>
2022-02-24 16:57:41 +0500

    upadte example to distribute card unique

--- lib/example.rb
+++ lib/example.rb
@@ -12,17 +12,19 @@ while flag do
             while list.length() != 5 do
                 card = face_values.sample + suit_values.sample
                 for h_l in hand_list do 
-                    if !h_l.include?(card.to_s)
+                    if !list.include?(card.to_s)
                         list.append(face_values.sample + suit_values.sample)
                     end
-                end
             end
         end
-        hand_list.append(list)
+        hand_list.append(PokerHand.new(list))
     end
 
-    for one_hand in hand_list do
-        PokerHand.new(one_hand)
+    for hand in hand_list do
+        if ["Straight Flush", "Royal Flush", "Four of a kind"].include? hand.check_rank()
+            flag = false
+        end
+        puts hand.check_rank() 
     end
 
     count += 1
