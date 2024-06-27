3953ca7f6eb48e4f1ca84957595641da701eb369
InnovationGreen <haffizaliraza@gmail.com>
2024-06-28 03:43:19 +0500

    Update poker_hand.rb

--- lib/poker/poker_hand.rb
+++ lib/poker/poker_hand.rb
@@ -1,106 +1,87 @@
 class PokerHand
+  attr_reader :cards
 
-    attr_reader :cards
+  ACTIONS = [
+    ['Royal Flush',     :royal_flush? ],
+    ['Straight Flush',  :straight_flush? ],
+    ['Four of a kind',  :four_of_a_kind? ],
+    ['Full house',      :full_house? ],
+    ['Flush',           :flush? ],
+    ['Straight',        :straight? ],
+    ['Three of a kind', :three_of_a_kind? ],
+    ['Two pair',        :two_pair? ],
+    ['Pair',            :pair? ],
+    ['Highest Card',    :highest_card? ]
+  ]
 
-    ACTIONS = [
-        ['Royal Flush',     :royal_flush? ],
-        ['Straight Flush',  :straight_flush? ],
-        ['Four of a kind',  :four_of_a_kind? ],
-        ['Full house',      :full_house? ],
-        ['Flush',           :flush? ],
-        ['Straight',        :straight? ],
-        ['Three of a kind', :three_of_a_kind?],
-        ['Two pair',        :two_pair? ],
-        ['Pair',            :pair? ],
-        ['Highest Card',    :highest_card? ]
-    ]
+  def initialize(cards)
+    build_cards(cards)
+  end
 
-    def build_cards(cards)
-        if (cards.is_a? Array) and cards.length() == 5
-            @cards = cards.map do  |c| 
-                Card.new(c.to_s)
-            end
-        else
-            @cards = []
-        end
+  def build_cards(cards)
+    if cards.is_a?(Array) && cards.length == 5
+      @cards = cards.map { |c| Card.new(c.to_s) }
+    else
+      @cards = []
     end
+  end
 
-    def royal_flush?
-        return (straight_flush?() and [10,11,12,13,14] == @cards.map{|c| c.face }.sort())
-    end
+  def royal_flush?
+    straight_flush? && @cards.map(&:face).sort == [10, 11, 12, 13, 14]
+  end
 
-    def straight_flush?
-        return (flush?() and straight?())
-    end
+  def straight_flush?
+    flush? && straight?
+  end
 
-    def four_of_a_kind?
-        hash = Hash.new(0)
-        @cards.map{ |c| hash[c.face] += 1 }
-        return hash.key(4) ? hash.key(4) : false
-    end
+  def four_of_a_kind?
+    count_faces(4)
+  end
 
-    def full_house?
-        hash = Hash.new(0)
-        @cards.map{ |c| hash[c.face] += 1 }
-        return (hash.key(2)) && hash.key(3) ? true : false
-    end
+  def full_house?
+    count_faces(3) && count_faces(2)
+  end
 
-    def flush?
-        hash = Hash.new(0)
-        @cards.map{ |c| hash[c.suit] += 1 }
-        return hash.key(5) ? hash.key(5) : false
-    end
+  def flush?
+    count_suits(5)
+  end
 
-    def straight?
-        return @cards.map{|c| c.face  }.sort().each_cons(2).all? { |x,y| y == x + 1 }
-    end
+  def straight?
+    @cards.map(&:face).sort.each_cons(2).all? { |x, y| y == x + 1 }
+  end
 
-    def three_of_a_kind?
-        hash = Hash.new(0)
-        @cards.map{ |c| hash[c.face] += 1 }
-        return hash.key(3) ? hash.key(3) : false
-    end
+  def three_of_a_kind?
+    count_faces(3)
+  end
 
-    def two_pair?
-        hash = Hash.new(0)
-        @cards.map{ |c| hash[c.face] += 1 }
-        return (hash.select{|k, v| v ==  2}.keys.length() == 2) ? hash.select{|k, v| v ==  2}.keys : false
-    end
+  def two_pair?
+    pairs = @cards.group_by(&:face).select { |_, group| group.length == 2 }.keys
+    pairs.length == 2 ? pairs : false
+  end
 
-    def pair?
-        hash = Hash.new(0)
-        @cards.map{ |c| hash[c.face] += 1 }
-        return hash.key(2) ? hash.key(2) : false
-    end
+  def pair?
+    count_faces(2)
+  end
 
+  def highest_card
+    @cards.max_by(&:face).face rescue false
+  end
 
-    def highest_card?
-        begin
-            high_card = @cards.max_by { |c|  c.face }
-            return high_card.face
-        rescue => exception
-            return false
-        end
-    end
+  def check_rank
+    ACTIONS.find { |name, method| send(method) }&.first
+  end
 
-    
-    def initialize(cards)
-        build_cards(cards)
-    end
+  def check_all_cards
+    @cards.map { |card| card.get_suit_by_value(card.suit) + card.get_face_by_value(card.face) }
+  end
 
-    def check_rank()
-        ACTIONS.map{ |ac| 
-            (method(ac[1]).call)? ac[0] : false
-        }.find { |v| v }
-    end
+  private
 
-    def check_all_cards()
-        cards_list = []
-        for card in @cards do
-            cards_list.append( card.get_suit_by_value(card.suit) + card.get_face_by_value(card.face) )
-        end
-        return cards_list
-    end
+  def count_faces(count)
+    @cards.group_by(&:face).any? { |_, group| group.length == count }
+  end
 
+  def count_suits(count)
+    @cards.group_by(&:suit).any? { |_, group| group.length == count }
+  end
 end
-
